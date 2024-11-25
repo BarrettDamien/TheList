4f69a489849bcafeeae87997d0d4c9b3ee2df56d
Zaedriel <zaedriel@gmail.com>
2024-11-25 22:16:32 +0000

    Update database.js

--- database.js
+++ database.js
@@ -6,14 +6,14 @@ const { Sequelize } = require('sequelize')
     logging: console.log,
 }) */
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_URL || 'theListDB', // Use DATABASE_URL from environment variables for production
-    process.env.DATABASE_USER || 'Zaedriel', // Local dev fallback
-    process.env.DATABASE_PASSWORD || 'jme6vzx0kgh7gpd_EQH', // Local dev fallback
+/* const sequelize = new Sequelize(
+    process.env.DATABASE_URL, // Use DATABASE_URL from environment variables for production
+    process.env.DATABASE_USER, 
+    process.env.DATABASE_PASSWORD, 
     {
-        host: process.env.DATABASE_HOST || '127.0.0.1', // Local dev fallback
+        host: process.env.DATABASE_HOST, 
         dialect: 'postgres',
-        port: process.env.DATABASE_PORT || 5432, // PostgreSQL default port
+        port: process.env.DATABASE_PORT, 
         logging: console.log, // Optional: set to `false` to disable query logging
         dialectOptions: process.env.DATABASE_URL ? {
         ssl: {
@@ -21,6 +21,28 @@ const sequelize = new Sequelize(
             rejectUnauthorized: false // For Heroku SSL configuration
         }
         } : {}
+    }
+) */
+
+const sequelize = process.env.DATABASE_URL ? new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: console.log,
+    dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false, // Necessary for Heroku SSL configuration
+    },
+    },
+})
+: new Sequelize(
+    process.env.DATABASE_NAME || 'theListDB', // Default to 'theListDB' for local dev if not set
+    process.env.DATABASE_USER || 'your_local_db_user', // Local fallback username
+    process.env.DATABASE_PASSWORD || 'your_local_db_password', // Local fallback password
+    {
+    host: process.env.DATABASE_HOST || '127.0.0.1', // Default to localhost for local dev
+    dialect: 'postgres',
+    port: process.env.DATABASE_PORT || 5432, // Default PostgreSQL port
+    logging: console.log, // Optional: set to `false` to disable query logging
     }
 )
 
