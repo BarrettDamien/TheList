8a81b31812834047341543fd906f8f1fff1cd527
Zaedriel <zaedriel@gmail.com>
2024-12-03 21:23:48 +0000

    Theme Designed
    Decided on synthwave style

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,18 @@ var router = express.Router();
 const watchlistRoutes = require('./watchlist');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/* router.get('/', function(req, res, next) {
   if(req.user) {
     res.render('index', { username: req.user.username });
   } else {
     res.redirect('/auth/signup') // adjusts where unauthed users are pointed to 
   }
+}); */
+
+router.get('/', function(req, res, next) {
+  res.render('index', { username: req.user ? req.user.username : null });
 });
 
-router.use('/watchlist', watchlistRoutes);
+/* router.use('/watchlist', watchlistRoutes); */
 
 module.exports = router;
